fc43e2c5ead418413a6bfb559e0483a92f1701a8
mitchellmar <48227621+mitchellmar@users.noreply.github.com>
2020-12-03 18:33:42 -0800

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,1 +1,54 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
+import javax.swing.*;
+import javax.imageio.*;
+
+public class GamePanel extends JPanel {
+    private int score = 0;
+    private boolean started = false;
+    private boolean ended = false;
+    
+   public TilePanel() {
+    	try {
+    		asteroid1 = ImageIO.read(new File("asteroid1.png"));
+    		earth = ImageIO.read(new File("earth.png"));
+    	} catch (IOException e) {
+    	}
+        setBackground(Color.BLACK);
+        addKeyListener(new PacManKeyListener());
+        setFocusable(true);
+        requestFocusInWindow();
+        for (int i = 0; i < 5; i++) {
+        	words.add(pick());
+        }
+        int DELAY = 10;
+        ActionListener updater = new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+            	if (!started || ended) {
+            		return;            		
+            	}
+                for (int i = 0; i < words.size(); i++) {
+                	Word word = words.get(i);
+                	word.strX += word.move;
+	                if (word.strX >= getWidth()) {
+	                	score -= 5;
+		            	if (score <= -50) {
+		            		ended = true;
+		            	}
+	                	word = pick();
+	                	words.set(i, word);
+	                }
+                }
+                repaint();
+            }
+        };
+        Timer tim = new Timer(DELAY, updater);
+        tim.start();        
+    }
+}
