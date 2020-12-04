52bb2281986aba0b3df8502a23b77c737990777c
mitchellmar <48227621+mitchellmar@users.noreply.github.com>
2020-12-03 18:29:19 -0800

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,1 +1,18 @@
 
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+  
+    private GamePanel panel;
+
+    public GameFrame() {
+        setSize(1000, 600);
+        setTitle("Protect Earth");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new TilePanel();
+        add(panel, BorderLayout.CENTER);
+    }
+  
+}
+
