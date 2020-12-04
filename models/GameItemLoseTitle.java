8e6bdd88940e8dbf0daf5c0bd8f0a40c3e28968c
mitchellmar <48227621+mitchellmar@users.noreply.github.com>
2020-12-04 11:31:44 -0800

    Create GameItemLoseTitle.java

---
+++ models/GameItemLoseTitle.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+public class GameItemLoseTitle extends JComponent {
+
+	// Declare final variables
+	private static final long serialVersionUID = 1L;
+	
+	// Item states
+	private int score;
+	
+	public GameItemLoseTitle() {}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+        g.setColor(Color.BLUE);
+        g.drawString("YOU LOST! EARTH WAS DESTROYED...", 200, 200);
+        g.setColor(Color.RED);
+        g.drawString("YOUR SCORE WAS " + score, 200, 250);
+        g.setColor(Color.GREEN);
+        g.drawString("HIT SPACEBAR TO RESTART GAME!", 200, 300);
+		
+	}
+	
+	public void setScore(int finalScore) {
+		this.score=finalScore;
+	}
+	
+}
