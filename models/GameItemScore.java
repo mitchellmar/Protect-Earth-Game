7279f02842c4a26bbb4670b9c35931a8577dd3a2
mitchellmar <48227621+mitchellmar@users.noreply.github.com>
2020-12-04 11:11:48 -0800

    Create GameItemScore.java

---
+++ models/GameItemScore.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+public class GameItemScore extends JComponent {
+
+	// Declare final variables
+	private static final long serialVersionUID = 1L;
+
+	// Item states
+	private int score;
+	
+	public GameItemScore() {}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+        g.drawString("Score: " + score, getWidth() - 80, 10);
+	}
+	
+	public void setScore(int newScore) {
+		this.score = newScore;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+}
