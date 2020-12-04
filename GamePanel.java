48dffbf5225b23f9a9ce1379d7329b5d1b6ba131
mitchellmar <48227621+mitchellmar@users.noreply.github.com>
2020-12-03 20:19:28 -0800

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -5,16 +5,26 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
 import javax.swing.*;
 import javax.imageio.*;
 
 public class GamePanel extends JPanel {
     private int score = 0;
+    private String typed = "";
+    private ArrayList<Word> words = new ArrayList<>();
+    private BufferedImage asteroid1;
+    private BufferedImage earth;
     private boolean started = false;
     private boolean ended = false;
+    private GameWordPicker pick1 = new GameWordPicker();
     
-   public TilePanel() {
+    class Word {
+    	String str = "";
+    	int move = 1;
+    	int strX = 0;
+    }
+   
+   public GamePanel() {
     	try {
     		asteroid1 = ImageIO.read(new File("asteroid1.png"));
     		earth = ImageIO.read(new File("earth.png"));
@@ -25,7 +35,7 @@ public class GamePanel extends JPanel {
         setFocusable(true);
         requestFocusInWindow();
         for (int i = 0; i < 5; i++) {
-        	words.add(pick());
+        	words.add(pick1.pick());
         }
         int DELAY = 10;
         ActionListener updater = new ActionListener() {
@@ -41,7 +51,7 @@ public class GamePanel extends JPanel {
 		            	if (score <= -50) {
 		            		ended = true;
 		            	}
-	                	word = pick();
+	                	word = pick1.pick();
 	                	words.set(i, word);
 	                }
                 }
@@ -49,6 +59,46 @@ public class GamePanel extends JPanel {
             }
         };
         Timer tim = new Timer(DELAY, updater);
-        tim.start();        
+        tim.start();
     }
+   
+   class PacManKeyListener extends KeyAdapter {
+       public void keyPressed(KeyEvent event) {
+           char keyChar = event.getKeyChar();
+           if (!Character.isAlphabetic(keyChar) && keyChar != ' ') {
+               return;
+           }
+           if (keyChar == ' ') {
+           	typed = "";
+           	if (!started) {
+           		started = true;
+           	}
+           	if (ended) {
+           		ended = false;
+           		score = 0;
+           	}
+           } else {
+           	typed += keyChar;
+           }
+           boolean same = false;
+           for (int i = 0; i < words.size(); i++) {
+           	Word word = words.get(i);
+           	if (typed.equals(word.str)) {
+	        		same = true;
+           		score += 10;
+	            	word = pick1.pick();
+	            	words.set(i, word);
+	            	if (score >= 100) {
+	            		ended = true;
+	            	}
+	        	}
+           }
+           if (same) {
+       		typed = "";
+           }
+           repaint();
+       }
+   }
+   
+   
 }
