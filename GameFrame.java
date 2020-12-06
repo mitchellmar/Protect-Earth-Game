c26eb868496b6a851af051587710f77cb97b6c98
richardnguyen99 <richard.ng0616@gmail.com>
2020-12-05 17:11:10 -0800

    refactored

--- GameFrame.java
+++ GameFrame.java
@@ -3,15 +3,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
-  
-    private GamePanel panel;
+
+    private GameScene panel;
 
     public GameFrame() {
         setSize(1000, 600);
         setTitle("Protect Earth");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel = new GamePanel();
+        panel = new GameScene();
         add(panel, BorderLayout.CENTER);
     }
-  
+
 }
